448fa91eed95befc356d198452b962f364f232cb
Ragil <work.ragil@gmail.com>
2022-08-30 03:56:21 +0700

    Update

--- prod/script/script-prod.sh
+++ prod/script/script-prod.sh
@@ -1,8 +1,8 @@
 sudo rsync -av * /go-trivia/
-sudo docker build /go-trivia/prod/frontend/. -t mraagil/trivia-frontend:prod
-sudo docker build /go-trivia/prod/backend/. -t mraagil/trivia-backend:prod
+sudo docker build /go-trivia/frontend/. -t mraagil/trivia-frontend:prod
+sudo docker build /go-trivia/backend/. -t mraagil/trivia-backend:prod
 sudo docker push mraagil/trivia-backend:prod
 sudo docker push mraagil/trivia-frontend:prod
 su ubuntu -c 'kubectl create namespace prod'
-su ubuntu -c 'cd /go-trivia/prod && make upgrade namespace=prod'
+su ubuntu -c 'cd /go-trivia/prod && make upgrade namespace=prod frontendhpa=true backendhpa=true'
 #su ubuntu -c 'cd /go-trivia/prod && make uninstall namespace=prod && make install namespace=prod'
--- qa/script/script-qa.sh
+++
@@ -1,8 +0,0 @@
-sudo rsync -av * /go-trivia/
-sudo docker build /go-trivia/qa/frontend/. -t mraagil/trivia-frontend:qa
-sudo docker build /go-trivia/qa/backend/. -t mraagil/trivia-backend:qa
-sudo docker push mraagil/trivia-backend:qa
-sudo docker push mraagil/trivia-frontend:qa
-su ubuntu -c 'kubectl create namespace qa'
-su ubuntu -c 'make install namespace=qa'
-su ubuntu -c 'cd /go-trivia/qa && make uninstall namespace=qa && make install namespace=qa'
