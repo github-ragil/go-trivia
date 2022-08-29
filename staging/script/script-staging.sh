7f51e54c83de16d60382a03aa00e721400cac4e6
Ragil <work.ragil@gmail.com>
2022-08-29 18:46:45 +0700

    Update

---
+++ staging/script/script-staging.sh
@@ -0,0 +1,6 @@
+sudo rsync -av * /go-trivia/
+sudo docker build /go-trivia/frontend/. -t mraagil/trivia-frontend:staging
+sudo docker build /go-trivia/backend/. -t mraagil/trivia-backend:staging
+sudo docker push mraagil/trivia-backend:staging
+sudo docker push mraagil/trivia-frontend:staging
+su ubuntu -c 'cd /go-trivia/staging && make uninstall && make install'
