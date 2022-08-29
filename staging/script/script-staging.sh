031832913a0a4ee66c7896129b2f559cb8f18dc4
Ragil <work.ragil@gmail.com>
2022-08-30 06:13:54 +0700

    update

--- staging/script/script-staging.sh
+++ staging/script/script-staging.sh
@@ -1,8 +1,7 @@
 sudo rsync -av * /go-trivia/
-sudo docker build /go-trivia/staging/frontend/. -t mraagil/trivia-frontend:staging
-sudo docker build /go-trivia/staging/backend/. -t mraagil/trivia-backend:staging
+sudo docker build /go-trivia/frontend/. -t mraagil/trivia-frontend:staging
+sudo docker build /go-trivia/backend/. -t mraagil/trivia-backend:staging
 sudo docker push mraagil/trivia-backend:staging
 sudo docker push mraagil/trivia-frontend:staging
 su ubuntu -c 'kubectl create namespace staging'
-su ubuntu -c 'make install namespace=staging'
-su ubuntu -c 'cd /go-trivia/staging && make uninstall namespace=staging && make install namespace=staging'
+su ubuntu -c 'cd /go-trivia/staging && make install namespace=staging'
