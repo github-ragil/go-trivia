bdeadbef1695bc15a533a0e7f0853477842ebc0f
Ragil <work.ragil@gmail.com>
2022-08-29 20:38:34 +0700

    Update

--- prod/script/script-prod.sh
+++ prod/script/script-prod.sh
@@ -1,6 +1,6 @@
 sudo rsync -av * /go-trivia/
-sudo docker build /go-trivia/frontend/. -t mraagil/trivia-frontend:prod
-sudo docker build /go-trivia/backend/. -t mraagil/trivia-backend:prod
+sudo docker build /go-trivia/prod/frontend/. -t mraagil/trivia-frontend:prod
+sudo docker build /go-trivia/prod/backend/. -t mraagil/trivia-backend:prod
 sudo docker push mraagil/trivia-backend:prod
 sudo docker push mraagil/trivia-frontend:prod
 su ubuntu -c 'kubectl create namespace prod'
---
+++ qa/frontend/go.mod
@@ -0,0 +1,3 @@
+module github.com/lmnzr/triviafrontend
+
+go 1.12
