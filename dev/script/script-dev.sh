6ab2267ff9d828e10fe15ce8de74ed0ddd9ed3ee
Ragil <work.ragil@gmail.com>
2022-08-30 05:53:52 +0700

    Update script-dev.sh

--- dev/script/script-dev.sh
+++ dev/script/script-dev.sh
@@ -4,4 +4,4 @@ sudo docker build /go-trivia/backend/. -t mraagil/trivia-backend:dev
 sudo docker push mraagil/trivia-backend:dev
 sudo docker push mraagil/trivia-frontend:dev
 su ubuntu -c 'kubectl create namespace dev'
-su ubuntu -c 'cd /go-trivia/dev && && make install namespace=dev && make upgrade namespace=dev'
+su ubuntu -c 'cd /go-trivia/dev && make install namespace=dev && make upgrade namespace=dev'
