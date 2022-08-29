71366b047460177a22d93e2c288b259e81cbbf56
Ragil <work.ragil@gmail.com>
2022-08-29 18:28:37 +0700

    Updated

--- script/build-docker.sh
+++ script/build-docker.sh
@@ -1,6 +1,6 @@
 sudo rsync -av * /go-trivia/
 sudo docker build /go-trivia/frontend/. -t mraagil/trivia-frontend:latest
 sudo docker build /go-trivia/backend/. -t mraagil/trivia-backend:latest
-sudo docker push mraagil/trivia-backend:kitabisa
-sudo docker push mraagil/trivia-frontend:kitabisa
+sudo docker push mraagil/trivia-backend:latest
+sudo docker push mraagil/trivia-frontend:latest
 su ubuntu -c 'cd /go-trivia && make uninstall && make install'
