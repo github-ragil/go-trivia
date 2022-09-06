8fe3a80ede2d767fb62f8853d997c990631c33d1
Ragil <work.ragil@gmail.com>
2022-09-06 08:28:33 +0700

    Update script-prod.sh

--- prod/script/script-prod.sh
+++ prod/script/script-prod.sh
@@ -3,5 +3,4 @@ sudo docker build /go-trivia/frontend/. -t mraagil/trivia-frontend:prod
 sudo docker build /go-trivia/backend/. -t mraagil/trivia-backend:prod
 sudo docker push mraagil/trivia-backend:prod
 sudo docker push mraagil/trivia-frontend:prod
-su ubuntu -c 'kubectl create namespace prod'
-su ubuntu -c 'cd /go-trivia/prod && make install namespace=prod frontendhpa=true backendhpa=true && make upgrade namespace=prod frontendhpa=true backendhpa=true'
+cd /go-trivia/prod && make install frontendhpa=true backendhpa=true'
