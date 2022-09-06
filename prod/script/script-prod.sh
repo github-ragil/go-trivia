66d1387497b995a1c1d1dba42a338279e408f375
Ragil <work.ragil@gmail.com>
2022-09-06 08:29:50 +0700

    Update script-prod.sh

--- prod/script/script-prod.sh
+++ prod/script/script-prod.sh
@@ -3,4 +3,4 @@ sudo docker build /go-trivia/frontend/. -t mraagil/trivia-frontend:prod
 sudo docker build /go-trivia/backend/. -t mraagil/trivia-backend:prod
 sudo docker push mraagil/trivia-backend:prod
 sudo docker push mraagil/trivia-frontend:prod
-cd /go-trivia/prod && make install frontendhpa=true backendhpa=true'
+su ubuntu -c 'cd /go-trivia/prod && make install frontendhpa=true backendhpa=true'
